4ffe2dba9215dc6cd692425cd981cc70eaf2e4c1
wanghaoxing <wang.haoxing@synyi.com>
2019-02-23 16:49:56 +0800

    add数组

---
+++ src/DataStructureAlgorithm/array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutOfRangeException();
+            Console.ReadKey();
+        }
+
+        private static void  OutOfRangeException()
+        {
+            int i = 0;
+            var arr = new int[3];
+            for (; i <= 3; i++)
+            {
+                arr[i] = 0;
+                Console.WriteLine("hello world\n");
+            }
+        }
+    }
+}
