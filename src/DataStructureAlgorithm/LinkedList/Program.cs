f21adbd1c079ff441c0dcd21f9b6eb4642d04554
wanghaoxing <wang.haoxing@synyi.com>
2019-03-01 17:54:09 +0800

    链表(上)

---
+++ src/DataStructureAlgorithm/LinkedList/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void PrintLinkedList<T>(SingleLinkedList<T> list) where T : IComparable<T>
+        {
+            if (list == null) return;
+
+            var p = list.First;
+            while (p != null)
+            {
+                System.Console.WriteLine(p.Value);
+                p = p.Next;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var lru = new LruLinkedList(5);
+            #region LRU_Set_Value_When_Not_Existed
+            //lru.Set(1);
+            //lru.Set(3);
+            //lru.Set(5);
+            //lru.Set(7);
+            //lru.Set(9);
+            #endregion
+            #region LRU_Set_Value_When_Existed
+            //lru.Set(1);
+            //lru.Set(3);
+            //lru.Set(5);
+            //lru.Set(7);
+            //lru.Set(3);
+            #endregion
+            #region LRU_Set_Value_When_Full
+            lru.Set(1);
+            lru.Set(3);
+            lru.Set(5);
+            lru.Set(7);
+            lru.Set(9);
+            lru.Set(8);
+            #endregion
+            var list = lru.LikLinkedList;
+
+            PrintLinkedList(list);
+
+            Console.ReadKey();
+        }
+
+    }
+
+    public class LruLinkedList
+    {
+        private readonly int _capacity;
+        public SingleLinkedList<int> LikLinkedList=new SingleLinkedList<int>();
+            public LruLinkedList(int capacity=10)
+        {
+            _capacity = capacity;
+        }
+
+        public void Set(int value)
+        {
+            var deleteNode = LikLinkedList.Delete(value);
+            if (deleteNode != null)
+            {
+                LikLinkedList.Insert(1, value);
+                return;
+            }
+
+            if (LikLinkedList.Length == _capacity)
+            {
+                LikLinkedList.Delete(LikLinkedList.Length);
+            }
+            LikLinkedList.Insert(1, value);
+        }
+
+    }
+}
---
+++ src/DataStructureAlgorithm/LinkedList/SingleLinkedList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LinkedList
+{
+    public class SingleLinkedList<T> where T : IComparable<T>
+    {
+        public SingleLinkedList()
+        {
+            Head = new ListNode<T>(default(T));
+        }
+
+        public SingleLinkedList(params T[] list)
+        {
+            Head = new ListNode<T>(default(T));
+            if (list == null) return;
+            var p = Head;
+            foreach (var item in list)
+            {
+                var q = new ListNode<T>(item);
+                p.Next = q;
+                p = q;
+            }
+
+            Length = list.Length;
+        }
+        public ListNode<T> First => Head.Next;
+        public ListNode<T> Head { get; }
+        public int Length { get; private set; }
+
+        public ListNode<T> Insert(int position, T newElem)
+        {
+            if (position < 1 || position > Length + 1)
+            {
+                throw new IndexOutOfRangeException("Position must be in bound of list");
+            }
+
+            var p = Head;
+            int j = 1;
+            while (p != null && j < position)
+            {
+                p = p.Next;
+                ++j;
+
+            }
+            var newNode = new ListNode<T>(newElem);
+            newNode.Next = p.Next;
+            p.Next = newNode;
+            Length++;
+            return newNode;
+        }
+
+        public ListNode<T> Delete(T value)
+        {
+            ListNode<T> cur = Head;
+            while (cur.Next!=null&&cur.Next.Value.CompareTo(value)!=0)
+            {
+                cur = cur.Next;
+            }
+            if (cur.Next == null) return null;
+
+            var q = cur.Next;
+            cur.Next = q.Next;
+
+            Length--;
+
+            return q;
+        }
+
+        public ListNode<T> Delete(int position)
+        {
+            if (position < 1 || position > Length)
+            {
+                return null;
+            }
+
+            var p = First;
+            int j = 1;
+            while (p != null && j < position - 1)
+            {
+                p = p.Next;
+                ++j;
+            }
+
+            var q = p.Next;
+            p.Next = q.Next;
+
+            Length--;
+
+            return q;
+        }
+    }
+
+    public class ListNode<T>
+    {
+        public ListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+        public ListNode<T> Next { get; set; }
+    }
+}
